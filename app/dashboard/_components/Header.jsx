f86dc2d13c07f0e8b249b0e430e6d6698ef41e43
Pritish <pritishmahajan12@gmail.com>
2024-07-13 00:46:12 +0530

    InterviewList added to dashboard and About Page

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect } from 'react'
 
@@ -11,12 +12,15 @@ function Header() {
     },[])
   return (
     <div className='flex p-3 justify-between items-center bg-secondary shadow'>
-      <Image src={'/logo.svg'} width={100} height={70} alt='logo'/>
+      <h1 className=' text-2xl font-extrabold'>AcePrep <span className=' text-primary'>AI</span></h1>
       <ul className='hidden sm:flex gap-6'>
-        <li className={` hover:text-primary hover:font-bold transition-all cursor-pointer ${(path==='/dashboard')?'text-primary font-semibold':''}`}>Dashboard</li>
+        <Link href={'/dashboard'}>
+        <li className={` hover:text-primary hover:font-bold transition-all cursor-pointer ${(path==='/dashboard')?'text-primary font-semibold':''}`}>Dashboard</li></Link>
         <li className={` hover:text-primary hover:font-bold transition-all cursor-pointer ${(path==='/dashboard/question')?'text-primary font-semibold':''}`}>Questions</li>
         <li className={` hover:text-primary hover:font-bold transition-all cursor-pointer ${(path==='/dashboard/upgrade')?'text-primary font-semibold':''}`}>Upgrade</li>
+        <Link href={'/dashboard/about'}>
         <li className={` hover:text-primary hover:font-bold transition-all cursor-pointer ${(path==='/dashboard/about')?'text-primary font-semibold':''}`}>About</li>
+        </Link>
       </ul>
       <UserButton/>
     </div>
